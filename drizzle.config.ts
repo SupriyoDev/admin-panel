c9bf62b169a06b6bea7df4e71be8200e51c232ac
supriyo maji <supriyamaji.math@gmail.com>
2025-03-28 11:44:26 +0530

    fixed database secret

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,5 @@
-import "dotenv/config";
+import { config } from "dotenv";
+config();
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
@@ -6,6 +7,6 @@ export default defineConfig({
   schema: "./drizzle/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: "postgresql://ueb6hu:xau_iInCMLHMH3duHUCvufsGbDGe2vLWr5Cs0@us-east-1.sql.xata.sh/infotech:main?sslmode=require",
+    url: process.env.NEXT_PUBLIC_DATABASE_URL!,
   },
 });
